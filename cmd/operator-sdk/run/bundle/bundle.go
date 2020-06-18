343c5f66880648e23d065497cfaf33541246988a
jesus m. rodriguez <jesusr@redhat.com>
2020-06-18 00:23:16 -0400

    add bundle image argument validation

--- cmd/operator-sdk/run/bundle/bundle.go
+++ cmd/operator-sdk/run/bundle/bundle.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	k8sinternal "github.com/operator-framework/operator-sdk/internal/util/k8sutil"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
@@ -24,7 +25,7 @@ type BundleCmd struct {
 func (c *BundleCmd) AddToFlagSet(fs *pflag.FlagSet) {
 	fs.StringVar(&c.Namespace, "namespace", "",
 		"Specifies the namespace to install the operator. It will default to the KUBECONFIG context")
-	fs.StringVar(&c.IndexImage, "index-image", "",
+	fs.StringVar(&c.IndexImage, "index-image", "quay.io/operator-framework/upstream-opm-builder:latest",
 		"index image")
 	fs.StringVar(&c.InstallMode, "install-mode", "",
 		"the install mode")
@@ -36,20 +37,44 @@ func (c *BundleCmd) AddToFlagSet(fs *pflag.FlagSet) {
 func (c *BundleCmd) Run() error {
 
 	fmt.Println("Hello from bundle cmd")
+	fmt.Println(c.BundleImage)
 	fmt.Println(c.IndexImage)
 	fmt.Println(c.Namespace)
 	fmt.Println(c.InstallMode)
 	return nil
 }
 
+func (c *BundleCmd) validate(args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("a bundle image is a required argument")
+	}
+	return nil
+}
+
 func NewCmd() *cobra.Command {
 	c := &BundleCmd{}
-	c.BundleImage = "bundle:foo"
 
 	cmd := &cobra.Command{
 		Use:   "bundle",
 		Short: "Run an Operator organized in bundle format with OLM",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := c.validate(args); err != nil {
+				return fmt.Errorf("invalid command args: %v", err)
+			}
+
+			// assign the bundle image
+			c.BundleImage = args[0]
+
+			if !cmd.Flags().Changed("namespace") {
+				fmt.Println("namespace changed")
+				// we should probably allow folks to pass in their kubeconfig
+				// path
+				_, defaultNamespace, err := k8sinternal.GetKubeconfigAndNamespace("")
+				if err != nil {
+					return fmt.Errorf("error getting kubeconfig and default namespace: %v", err)
+				}
+				c.Namespace = defaultNamespace
+			}
 			if err := c.Run(); err != nil {
 				log.Fatalf("Failed to run operator: %v", err)
 			}
