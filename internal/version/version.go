87d847dac66caca040a051a467e2bdae138bc35a
jesus m. rodriguez <jesusr@redhat.com>
2021-03-16 13:49:26 -0400

    version version

--- internal/version/version.go
+++ internal/version/version.go
@@ -16,8 +16,8 @@ package version
 
 // var needs to be used instead of const for ldflags
 var (
-	Version           = "unknown"
-	GitVersion        = "unknown"
+	Version = "unknown"
+	// GitVersion        = "unknown"
 	GitCommit         = "unknown"
 	KubernetesVersion = "unknown"
 
